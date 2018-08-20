580f2fc93a901e82ee8363a5df34fcabb76ded65
Blockchain Help <31084431+blockchainhelp@users.noreply.github.com>
2018-08-21 00:02:25 +0530

    Add files via upload

---
+++ FakeQpContentItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using QA.Core.Data.QP;
+using Quantumart.QPublishing.Info;
+using Quantumart.QPublishing.Info;
+
+namespace QA.Core.Data.Tests.QP
+{
+    public class FakeQpContentItem : IQpContentItem
+    {
+        public ContentItem New(int contentId, IQpDbConnector cnn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ContentItem Read(int id, IQpDbConnector cnn)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id, IQpDbConnector cnn)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ServiceResult`1.cs
@@ -0,0 +1,40 @@
+﻿// Owners: Alexey Abretov, Nikolay Karlov
+
+using System;
+using System.Runtime.Serialization;
+
+namespace QA.Core.Service.Interaction
+{
+    /// <summary>
+    /// Класс, описывающий ответ от сервиса
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    [DataContract]
+    public class ServiceResult<TResult> : ServiceResult, IServiceResult<TResult>
+    {
+	    /// <summary>
+        /// Возвращаемый объект
+        /// </summary>
+        [DataMember]
+        public TResult Result { get; set; }
+
+		/// <summary>
+		/// Возвращает Result при условии что IsSucceeded==true и он не null
+		/// в противном случае бросает эксепшн
+		/// </summary>
+	    [IgnoreWhileDumping]
+		public TResult ResultEnsured
+	    {
+		    get
+		    {
+			    if (!IsSucceeded)
+				    throw new OperationExecutionException(Error);
+
+			    if (Result == null)
+				    throw new Exception("Result is null");
+
+			    return Result;
+		    }
+	    }
+    }
+}
